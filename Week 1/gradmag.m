e73dd62863019cf06449ba67f194b41f731439ba
tobyhijzen <tobyhijzen@gmail.com>
2012-11-07 18:33:27 +0100

    Added pictures of first derivatives of two pictures for different sigma. I show the orientation, and magnitude separately and combined in the quiver function.

--- Week 1/gradmag.m
+++ Week 1/gradmag.m
@@ -1,14 +1,16 @@
 function [magnitude,orientation] = gradmag(img,sigma)
-    Gd = gaussianDer(gaussian(sigma),sigma);
+    G = gaussian(sigma);
+    Gd = gaussianDer(G,sigma);
     L = (length(Gd)-1)/2;
-    Xmag = conv2(Gd,img);
-    Ymag = conv2(Gd',img);
-    Xmag = Xmag(:,1+L:end-L);
-    Ymag = Ymag(1+L:end-L,:);
+    Xmag = conv2(G',conv2(Gd,img));
+    Ymag = conv2(G,conv2(Gd',img));
+    Xmag = Xmag(1+L:end-L,1+L:end-L);
+    Ymag = Ymag(1+L:end-L,1+L:end-L);
     magnitude = sqrt(Xmag.^2 + Ymag.^2);
     orientation = atan2(Ymag,Xmag);
     stepsize = 10;
-    quiver(1:stepsize:size(img,1),1:stepsize:size(img,2), ...
-           Xmag(1:stepsize:end,1:stepsize:end), ...
-           Ymag(1:stepsize:end,1:stepsize:end),10);
+    figure(3)
+    quiver(5+1:stepsize:size(img,2),5+1:stepsize:size(img,1), ...
+           Xmag(5+1:stepsize:end,5+1:stepsize:end), ...
+           Ymag(5+1:stepsize:end,5+1:stepsize:end),2);
 end
--- Week 1/test.m
+++ Week 1/test.m
@@ -1,15 +1,18 @@
 %test
 function test
 im = imread('zebra.png');
-im = rgb2gray(im);
+if size(im,3) > 1
+    im = rgb2gray(im);
+end
 %imOut = gaussianConv('zebra.png',7,7);
 %imshow(imOut,[])
 
-[m,o] = gradmag(im,1);
+[m,o] = gradmag(im,3);
 
 figure(1)
 imshow(m,[])
 figure(2)
+hold on
 imshow(o,[-pi,pi])
 colormap(hsv)
 colorbar;
