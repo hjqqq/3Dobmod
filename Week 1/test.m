ede83a04d69bce5946ba202fc6b3501534a5f84a
tobyhijzen <tobyhijzen@gmail.com>
2012-11-07 14:12:48 +0100

    gradient pictures

--- Week 1/test.m
+++ Week 1/test.m
@@ -5,5 +5,11 @@ im = rgb2gray(im);
 %imOut = gaussianConv('zebra.png',7,7);
 %imshow(imOut,[])
 
-G = gaussian(5);
-Gd = gaussianDer(G,5);
+[m,o] = gradmag(im,1);
+
+figure(1)
+imshow(m,[])
+figure(2)
+imshow(o,[-pi,pi])
+colormap(hsv)
+colorbar;
