9712507a59e7b764dc435dee59384e3c55840d69
kriepy <k.rieping@gmail.com>
2012-11-16 12:09:11 +0100

    sift update

--- Week 2/runningSIFT.m
+++ Week 2/runningSIFT.m
@@ -9,9 +9,24 @@ im2 = single(im2);
 end
 %% Keypoints and orientation
 % f is a matrix, the first two columns is the center of the keypoints, the 3rd
-% colum is the size and the 4th the orientation
+% colum is the size and the 4th the orientation (we dont use orientation
+% for now)
 % d is the descriptor of the keypoints 
-[f,d] = vl_sift(im1);
+[m1,o1]=gradmag(im1,1);
+[m2,o2]=gradmag(im2,1);
+o1=o1+2*pi*(o1<0);
+o2=o2+2*pi*(o2<0);
+grad1(1,:,:)=m1;
+grad1(2,:,:)=o1;
+grad2(1,:,:)=m2;
+grad2(2,:,:)=o2;
+%the keypoints should be gained with harris, just for thesting now !!!!!!!!!!!!!
+[f1,~] = vl_sift(im1);
+[f2,~] = vl_sift(im2);
+d1 = vl_siftdescriptor(grad1,f1);
+d2 = vl_siftdescriptor(grad2,f2);
+
+
 % this is plotting some keypoints
 % imshow(im1,[]);
 % hold on;
@@ -27,11 +42,11 @@ end
 
 %% Matching two images
 [f2,d2]=vl_sift(im2); %same but then for second image
-[matches, scores] = vl_ubcmatch(d, d2) ;
+[matches, scores] = vl_ubcmatch(d1, d2) ;
 % this is plotting the matching points
 imshow(im1,[]);
 hold on;
-h1 = vl_plotframe(f(:,matches(1,:)));
+h1 = vl_plotframe(f1(:,matches(1,:)));
 set(h1,'color','y','linewidth',2) ;
 hold off;
 figure(2);
