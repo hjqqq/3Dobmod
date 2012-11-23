7fc68ccb3ae7ad33b9b3895e6900a8733216d778
tobyhijzen <tobyhijzen@gmail.com>
2012-11-23 14:23:55 +0100

    added comments and pics

--- Week 3/RANSAC.m
+++ Week 3/RANSAC.m
@@ -14,9 +14,13 @@ if nargin<4
     mode = 'affine';
 end
 
+%number of matches needed
 if strcmp(mode,'affine')
     npix = 3;
+elseif strcmp(mode,'projective')
+    npix = 4;
 else
+    display('unknown mode in RANSAC using projective')
     npix = 4;
 end
 bestInliers = 0;
@@ -24,8 +28,9 @@ bestLS = Inf;
 bestTM = NaN;
 
 for round=1:N
-
+    %determine transformation
     TM=maakMat(p1,p2,npix,mode);
+    
     % determine number of inliers
     inliers = 0;
     LS=0;
@@ -33,7 +38,7 @@ for round=1:N
     for i=1:size(p1,2)
         p2estimate = TM*[p1(:,i);1];
         nor=norm(p2estimate(1:2,:)./p2estimate(3,:)-p2(:,i));
-        if nor < 10
+        if nor < 10 %within a euclidean distance of 10 pixels
             inliers = inliers+1;
             LS=LS+nor;
             intin=[intin;i];
@@ -65,6 +70,7 @@ end
 end
 
 function TM = maakMat(p1,p2,npix,mode)
+    % randomly select set of points
     permutation = randperm(size(p1,2));
     p1p = p1(:,permutation(1:npix));
     p2p = p2(:,permutation(1:npix));
@@ -83,6 +89,7 @@ function TM = maakMat(p1,p2,npix,mode)
         T = [x(5);x(6)];
         TM = [M,T;0,0,1];
     else %'projective'
+        %construct linear system
         A = zeros(8,8);
         A(1:3:8,3)=1;
         A(2:3:8,6)=1;
@@ -92,6 +99,7 @@ function TM = maakMat(p1,p2,npix,mode)
         A(3:4,7:8)=-p2p(:,2)*p1p(:,2)';
         A(5:6,7:8)=-p2p(:,3)*p1p(:,3)';
         A(7:8,7:8)=-p2p(:,4)*p1p(:,4)';
+        %solve system
         x = [pinv(A)*p2p(:);1];
         x = reshape(x,3,3);
         TM=x';
--- Week 3/stitch.m
+++ Week 3/stitch.m
@@ -1,3 +1,12 @@
+% im3 = stitch(im1,im2,mode)
+%
+% INPUT
+% - im1: first image should be grayscale and of single precision
+% - im2: second image should be grayscale and of single precision
+% - mode: can be either 'affine' or 'projective'
+%
+% OUTPUT
+% - im3: stitched image
 function im3 = stitch(im1,im2,mode)
 
 if nargin<1
@@ -17,11 +26,12 @@ p2 = f2match(1:2,:);
 
 %perform RANSAC to find transformation
 A = RANSAC(5000,p1,p2,mode);
-%transform the 2cnd image
 
+%make t-form for both first and second image
 tA = maketform(mode,A');
 tI = maketform(mode,eye(3));
 
+%find new image size
 [h,w] = size(im1);
 [h2,w2] = size(im2);
 newcorners = A*[1,w,1,w;1,1,h,h;1,1,1,1];
@@ -31,6 +41,7 @@ maxXY = max([newcorners [w2;h2;1]],[],2);
 Xdata = [minXY(1), maxXY(1)];
 Ydata = [minXY(2), maxXY(2)];
 
+%transform both images to canvas and combine
 imt1 = imtransform(im1,tA,'Xdata',Xdata,'Ydata',Ydata,'FillValues',NaN);
 imt2 = imtransform(im2,tI,'Xdata',Xdata,'Ydata',Ydata,'FillValues',NaN);
 [nh, nw] = size(imt1);
