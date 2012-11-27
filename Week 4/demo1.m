bddecf392c900e4166e3cf82a4e96f40f2f49504
tobyhijzen <tobyhijzen@gmail.com>
2012-11-27 16:10:13 +0100

    improved flow

--- Week 4/demo1.m
+++ Week 4/demo1.m
@@ -1,14 +1,22 @@
+%function demo1
+%Shows a demo of the flow function on two images.
 function demo1()
 
-% im1 = imread('synth1.pgm');
-% im2 = imread('synth2.pgm');
-% [F,ind] = flow(im1,im2);
-% imshow(im1)
-% hold on
-% quiver(ind(:,:,1),ind(:,:,2),F(:,:,1),F(:,:,2),'m');
-% 
-% pause()
+%Show the flow in the synthesized image
+clf();
+im1 = imread('synth1.pgm');
+im2 = imread('synth2.pgm');
+[F,ind] = flow(im1,im2);
+imshow(im1)
+hold on
+quiver(ind(:,:,1),ind(:,:,2),F(:,:,1),F(:,:,2),'m');
 
+%pause process
+display('Press any key to continue')
+pause()
+clf();
+
+%Show the flow in the sphere image
 im1 = rgb2gray(imread('sphere1.ppm'));
 im2 = rgb2gray(imread('sphere2.ppm'));
 [F,ind] = flow(im1,im2);
--- Week 4/flow.m
+++ Week 4/flow.m
@@ -10,6 +10,9 @@
 %- ind: indexes of the flow vectors
 function [F,ind] = flow(im1,im2)
 
+im1 = double(im1);
+im2 = double(im2);
+% if no images are provided load standard images
 if nargin < 1
     im1 = imread('synth1.pgm');
     im2 = imread('synth2.pgm');
@@ -20,31 +23,34 @@ end
 hDevide = floor(h/15);
 wDevide = floor(w/15);
 
+%Calculate the center coordinates of the regions
 ind = zeros(hDevide,wDevide,2);
 ind(:,:,1) = repmat((0:wDevide-1)',1,hDevide)*15+7.5;
 ind(:,:,2) = repmat((0:hDevide-1),wDevide,1)*15+7.5;
 
-Ix = conv2(im1,[-1,0,1],'same');
-Iy = conv2(im1,[-1,0,1]','same');
+%Find image derivatives in x,y and t direction
+sigma =1;
+G = fspecial('gaussian',[1 2*ceil(3*sigma)+1],sigma);
+Gd = gaussianDer(G,sigma);
+Ix = conv2(im1,Gd,'same');
+Iy = conv2(im1,Gd','same');
 It = im2-im1;
 
+%For every patch find flow vector and store in F
 F = zeros(hDevide,wDevide,2);
 for i=0:hDevide-1
     for j=0:wDevide-1
+        % make a matrix consisting of derivatives along the patch
         A1 =Ix(i*15+1:(i+1)*15,j*15+1:(j+1)*15);
         A2 =Iy(i*15+1:(i+1)*15,j*15+1:(j+1)*15);
         A = [A1(:) , A2(:)];
+        % make b matrix consisting of derivatives in time
         b = It(i*15+1:(i+1)*15,j*15+1:(j+1)*15);
         b = b(:);
-        v = inv(A'*A) * A' * double(b);
+        %find b
+        v = pinv(A'*A) * A' * double(b);
         F(i+1,j+1,:) = v;
     end
 end
-
-% compute A At and b
-
-
-
-% visualize
 
 end
