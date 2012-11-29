03090887418d104e7bb3859ebf6c94d79bf3bd75
tobyhijzen <tobyhijzen@gmail.com>
2012-11-29 23:00:53 +0100

    there we go demo3 for part 2

---
+++ Week 4/demo3.m
@@ -0,0 +1,28 @@
+function demo3()
+
+
+%load points
+Points = importdata('model house\measurement_matrix.txt');
+
+[~,noPoints] = size(Points);
+Points = Points - repmat(mean(Points,2),1,noPoints);
+
+%singular value decomposition
+[U,W,V] = svd(Points);
+
+U = U(:,1:3);
+W = W(1:3,1:3);
+V = V(:,1:3);
+
+M = U*(W^0.5);
+S = (W^0.5)*V';
+
+plot3(S(1,:),S(2,:),S(3,:),'.')
+% for num = 1:101;
+%     imageLoc = ['model house\frame' num2str(num, '%08d') '.jpg'];
+%     im = double(imread(imageLoc))/255;
+%     imshow(im);
+%     hold on 
+%     plot(Points(num*2-1,:),Points(num*2,:),'b.');
+%     pause(0.1)
+% end
