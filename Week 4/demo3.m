6dfd0720066559ef8eafc1685939811db15deb8e
tobyhijzen <tobyhijzen@gmail.com>
2012-11-30 16:28:14 +0100

    structure from motion now working

--- Week 4/demo3.m
+++ Week 4/demo3.m
@@ -1,5 +1,6 @@
+%function demo3()
+%A demo of the structure from motion
 function demo3()
-
 
 %load points
 Points = importdata('model house\measurement_matrix.txt');
@@ -14,32 +15,19 @@ U = U(:,1:3);
 W = W(1:3,1:3);
 V = V(:,1:3);
 
-M = U*(W^0.5)
-size(M)
-L=M'*M
-C=chol(L)
-M=M*C
+M = U*(W^0.5);
 S = (W^0.5)*V';
-S=C^-1*S
 
-plot3(S(1,:),S(2,:),S(3,:),'.')
+%solve for affine ambiguity
+A1 = M(1:2,:);
+L0=pinv(A1'*A1);
+save('M','M')
 
-Ai = M(1:2,:);
-L=pinv(Ai'*Ai);
+L = lsqnonlin(@myfun,L0);
 
-%C=chol(L,'lower');
-%M = M*C;
-%S = pinv(C)*S;
+C = chol(L,'lower');
+M = M*C;
+S = pinv(C)*S;
 
-% Ai = M(1:2,1:2);
-% L = pinv(Ai'*Ai)
-% Ai * L * Ai'
-% C = chol(L,'lower');
-% for num = 1:101;
-%     imageLoc = ['model house\frame' num2str(num, '%08d') '.jpg'];
-%     im = double(imread(imageLoc))/255;
-%     imshow(im);
-%     hold on 
-%     plot(Points(num*2-1,:),Points(num*2,:),'b.');
-%     pause(0.1)
-% end
+%show the result
+plot3(S(1,:),S(2,:),S(3,:),'.');
